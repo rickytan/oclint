b5c23221bba988b05b8b630b50903843a9e881e3
Ricky <ricky.tan.xin@gmail.com>
2018-05-03 18:40:11 +0800

    feat: 去掉宏展开及 OC 属性自增的情况

--- oclint-rules/rules/style/BinaryOperatorStyleRule.cpp
+++ oclint-rules/rules/style/BinaryOperatorStyleRule.cpp
@@ -21,25 +21,41 @@ public:
 
     bool VisitBinaryOperator(clang::BinaryOperator *S) {
         SourceManager &sm = _carrier->getSourceManager();
-    
-        auto start = S->getOperatorLoc();
-        auto end = start.getLocWithOffset(S->getOpcodeStr().size() - 1);
 
-        // 双目操作符后面需要空格或回车
-        auto post = end.getLocWithOffset(1);
-        if (post.isValid()) {
-            const char *code = sm.getCharacterData(post);
-            if (code && string(" \n").find(code[0]) == string::npos) {
-                addViolation(start, end, this, "双目操作符后需要空格");
+        /*
+         ObjC 对像的属性自增会被编译器认为是双目操作符，如：
+         @code
+         @interface MyObject : NSObject
+         @property (assign) int bar;
+         @end
+         
+         MyObject *obj;
+         obj.bar ++;    // 编译后为 [obj setBar:[obj bar] + 1];
+         @endcode
+         
+         这时左右值的 SourceLocation 是一样的，因为是编译器展开的，并不是源码本身
+         */
+        if (S->getLHS()->getExprLoc() != S->getRHS()->getExprLoc()) {
+            
+            auto start = S->getOperatorLoc();
+            auto end = start.getLocWithOffset(S->getOpcodeStr().size() - 1);
+            
+            // 双目操作符后面需要空格或回车
+            auto post = end.getLocWithOffset(1);
+            if (post.isValid() && post.isFileID()) {
+                const char *code = sm.getCharacterData(post);
+                if (code && string(" \n").find(code[0]) == string::npos) {
+                    addViolation(start, end, this, "双目操作符后需要空格");
+                }
             }
-        }
-        
-        // 双目操作符前面需要空格或回车
-        auto pre = start.getLocWithOffset(-1);
-        if (pre.isValid()) {
-            const char *code = sm.getCharacterData(pre);
-            if (code && string(" \n").find(code[0]) == string::npos) {
-                addViolation(start, end, this, "双目操作符前需要空格");
+            
+            // 双目操作符前面需要空格或回车
+            auto pre = start.getLocWithOffset(-1);
+            if (pre.isValid() && pre.isFileID()) {
+                const char *code = sm.getCharacterData(pre);
+                if (code && string(" \n").find(code[0]) == string::npos) {
+                    addViolation(start, end, this, "双目操作符前需要空格");
+                }
             }
         }
         return true;
